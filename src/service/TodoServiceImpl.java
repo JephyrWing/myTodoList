c353412e9debaef0e81974143f55db2d9ca0c026
JephyrWing <gad03302@gmail.com>
2026-04-15 09:52:23 +0900

    #1 impl 생성, Map<String, List<Todo>>를 가지는 state 생성

---
+++ src/service/TodoServiceImpl.java
@@ -0,0 +1,38 @@
+package service;
+
+import vo.Todo;
+
+import java.util.List;
+import java.util.Map;
+
+public class TodoServiceImpl implements TodoService{
+    @Override
+    public void addTodo(String date, String time, String task) {
+
+    }
+
+    @Override
+    public List<Todo> getTodosByDate(String date) {
+        return List.of();
+    }
+
+    @Override
+    public Map<String, List<Todo>> getAllTodos() {
+        return Map.of();
+    }
+
+    @Override
+    public void updateTodo(String date, int index, String time, String task) {
+
+    }
+
+    @Override
+    public void deleteTodo(String date, int index) {
+
+    }
+
+    @Override
+    public void completeTodo(String date, int index) {
+
+    }
+}
---
+++ src/state/TodoState.java
@@ -0,0 +1,14 @@
+package state;
+
+import vo.Todo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TodoState {
+    private Map<String, List<Todo>> todoMap = new HashMap<>();
+
+    public TodoState() {
+    }
+}
